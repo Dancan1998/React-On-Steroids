2de4b82bd4f15344c91dc9202070baab75a28a5f
Dancan Chibole <dancankingstar@gmail.com>
2022-03-14 06:09:24 +0300

    useReducer hook

--- refresh/src/tutorial/6-useReducer/setup/index.js
+++ refresh/src/tutorial/6-useReducer/setup/index.js
@@ -1,10 +1,64 @@
-import React, { useState, useReducer } from 'react';
-import Modal from './Modal';
-import { data } from '../../../data';
+import React, { useState, useReducer } from "react";
+import Modal from "./Modal";
+import { data } from "../../../data";
 // reducer function
+const reducer = (state, action) => {
+  if (action.type === "TESTING") {
+    const newPeople = [...state.people, action.payload];
+    return {
+      ...state,
+      people: newPeople,
+      isModalOpen: false,
+      modalContent: "item added",
+    };
+  }
+  // if(action.type ==== 'N')
+  throw new Error("no matching action type");
+  // return state;
+};
+const defaultState = {
+  people: [],
+  isModalOpen: false,
+  modalContent: "",
+};
 
 const Index = () => {
-  return <h2>useReducer</h2>;
+  const [state, dispatch] = useReducer(reducer, defaultState);
+  const [name, setName] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (name) {
+      const newItem = { id: new Date().getTime().toString(), name };
+      dispatch({ type: "TESTING", payload: newItem });
+      setName("");
+    } else {
+      dispatch({ type: "no  value" });
+    }
+  };
+  return (
+    <>
+      {state.isModalOpen && <Modal modalContent={state.modalContent} />}
+      <form onSubmit={handleSubmit} className="form">
+        <div>
+          <input
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </div>
+        <div>
+          <button type="submit">add person</button>
+        </div>
+      </form>
+      {state.people.map((person) => {
+        return (
+          <div key={person.id}>
+            <h4>{person.name}</h4>
+          </div>
+        );
+      })}
+    </>
+  );
 };
 
 export default Index;
